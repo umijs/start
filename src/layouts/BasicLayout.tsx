19800a6b1b0bab556c360552f3699b35878098ae
chenshuai2144 <qixian.cs@outlook.com>
2021-04-29 18:54:54 +0800

    init form

--- src/layouts/BasicLayout.tsx
+++ src/layouts/BasicLayout.tsx
@@ -1,29 +1,158 @@
-import React from "react";
-import type { FC } from "react";
-import { Layout } from "antd";
-import { SettingFilled } from "@ant-design/icons";
-import styles from "./index.less";
+import React, { useRef, useContext, useState } from 'react';
+import type { FC } from 'react';
+import ProLayout, { DefaultFooter, PageContainer } from '@ant-design/pro-layout';
+import { Link, history } from 'alita';
+import { Input, Space, Tag } from 'antd';
+import { Menu } from 'antd';
+import ProCard from '@ant-design/pro-card';
+import ProProvider from '@ant-design/pro-provider';
 
-const { Header, Content, Sider } = Layout;
+const TagList: React.FC<{
+  value?: string[];
+  onChange?: (value: string[]) => void;
+}> = ({ value, onChange }) => {
+  const ref = useRef<Input | null>(null);
+  const [newTags, setNewTags] = useState<string[]>([]);
+  const [inputValue, setInputValue] = useState<string>('');
 
-interface PageProps {}
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setInputValue(e.target.value);
+  };
 
-const BasicLayout: FC<PageProps> = ({ children, location, history }) => {
+  const handleInputConfirm = () => {
+    let tempsTags = [...(value || [])];
+    if (inputValue && tempsTags.filter((tag) => tag === inputValue).length === 0) {
+      tempsTags = [...tempsTags, inputValue];
+    }
+    onChange?.(tempsTags);
+    setNewTags([]);
+    setInputValue('');
+  };
+
   return (
-    <Layout className={styles.center}>
-      <Header>
-        <div className={styles.logo}>
-          <img src="https://img.alicdn.com/tfs/TB1YHEpwUT1gK0jSZFhXXaAtVXa-28-27.svg" />
-          Umi Start
-        </div>
-        <div className={styles.user}>
-          <SettingFilled style={{ fontSize: "18px", marginRight: "15px" }} />
-        </div>
-      </Header>
-      <Layout style={{ padding: "0 24px 24px" }}>
-        <Content>{children}</Content>
-      </Layout>
-    </Layout>
+    <Space>
+      {(value || []).concat(newTags).map((item) => (
+        <Tag key={item}>{item}</Tag>
+      ))}
+      <Input
+        ref={ref}
+        type="text"
+        size="small"
+        style={{ width: 78 }}
+        value={inputValue}
+        onChange={handleInputChange}
+        onBlur={handleInputConfirm}
+        onPressEnter={handleInputConfirm}
+      />
+    </Space>
+  );
+};
+
+const BasicLayout: FC<{}> = ({ children, ...rest }) => {
+  const values = useContext(ProProvider);
+  return (
+    <ProLayout
+      logo="https://gw.alipayobjects.com/zos/antfincdn/bWi%26ZHzXn9/wheat.svg"
+      title="Start"
+      layout="top"
+      navTheme="light"
+      fixedHeader
+      contentWidth="Fluid"
+      footerRender={() => <DefaultFooter />}
+      {...rest}
+      onMenuHeaderClick={() => history.push('/')}
+      menuItemRender={(menuItemProps, defaultDom) => {
+        if (
+          menuItemProps.isUrl ||
+          !menuItemProps.path ||
+          location.pathname === menuItemProps.path
+        ) {
+          return defaultDom;
+        }
+        return <Link to={menuItemProps.path}>{defaultDom}</Link>;
+      }}
+    >
+      <PageContainer>
+        <ProCard split="vertical">
+          <ProCard
+            bodyStyle={{
+              paddingRight: 0,
+            }}
+            title="项目类型"
+            colSpan="300px"
+          >
+            <Menu
+              style={{
+                width: '100%',
+                height: '100%',
+              }}
+            >
+              <Menu.Item>
+                <img
+                  style={{
+                    width: '16px',
+                    display: 'inline-flex',
+                    height: '16px',
+                    verticalAlign: 'middle',
+                    marginRight: 8,
+                  }}
+                  src="https://img.alicdn.com/tfs/TB1zomHwxv1gK0jSZFFXXb0sXXa-200-200.png"
+                />
+                <span
+                  style={{
+                    display: 'inline-flex',
+                  }}
+                >
+                  Umi
+                </span>
+              </Menu.Item>
+              <Menu.Item>
+                <img
+                  style={{
+                    width: '16px',
+                    height: '16px',
+                    verticalAlign: 'middle',
+                    display: 'inline-flex',
+                    marginRight: 8,
+                  }}
+                  src="https://gw.alipayobjects.com/zos/rmsportal/KDpgvguMpGfqaHPjicRK.svg"
+                />
+                <span
+                  style={{
+                    display: 'inline-flex',
+                  }}
+                >
+                  Ant Design Pro
+                </span>
+              </Menu.Item>
+            </Menu>
+          </ProCard>
+          <ProCard title="配置" headerBordered>
+            <ProProvider.Provider
+              value={{
+                ...values,
+                valueTypeMap: {
+                  tags: {
+                    render: (text) => {
+                      return (
+                        <>
+                          {[text].flat(1).map((item) => (
+                            <Tag key={item}>{item}</Tag>
+                          ))}
+                        </>
+                      );
+                    },
+                    renderFormItem: (text, props) => <TagList {...props} {...props?.fieldProps} />,
+                  },
+                },
+              }}
+            >
+              {children}
+            </ProProvider.Provider>
+          </ProCard>
+        </ProCard>
+      </PageContainer>
+    </ProLayout>
   );
 };
 
--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -1,8 +1,8 @@
-import React from "react";
-import type { FC } from "react";
-import { useRequest } from "alita";
-import styles from "./index.less";
-import { getTempsListData } from "@/services/api";
+import React from 'react';
+import type { FC } from 'react';
+import { useRequest } from 'alita';
+import { StepsForm, BetaSchemaForm } from '@ant-design/pro-form';
+import { getTempsListData } from '@/services/api';
 
 interface PageProps {}
 
@@ -11,7 +11,209 @@ const IndexPage: FC<PageProps> = ({}) => {
   console.log(data);
 
   return (
-    <div className={styles.center}>Index Page,Hello {JSON.stringify(data)}</div>
+    <StepsForm>
+      <BetaSchemaForm
+        initialValues={{
+          project: {
+            name: 'umi',
+            version: '1.0.0',
+            description: '新的独角兽在这里启航',
+            keywords: ['umi'],
+            license: 'MIT',
+          },
+        }}
+        layoutType="StepForm"
+        title="项目信息配置"
+        columns={[
+          {
+            title: '项目名称',
+            dataIndex: ['project', 'name'],
+            formItemProps: {
+              rules: [
+                {
+                  required: true,
+                  message: '此项为必填项',
+                },
+              ],
+            },
+          },
+          {
+            title: '版本号',
+            width: 'sm',
+            dataIndex: ['project', 'version'],
+          },
+          {
+            title: '描述',
+            dataIndex: ['project', 'description'],
+            valueType: 'textarea',
+            formItemProps: {
+              rules: [
+                {
+                  required: true,
+                  message: '此项为必填项',
+                },
+              ],
+            },
+          },
+          {
+            title: '关键字',
+            dataIndex: ['project', 'keywords'],
+            valueType: 'tags',
+          },
+          {
+            title: '作者',
+            dataIndex: ['project', 'author'],
+            valueType: 'formList',
+            columns: [
+              {
+                valueType: 'group',
+                columns: [
+                  {
+                    title: '名称',
+                    dataIndex: 'name',
+                    width: 'xs',
+                    formItemProps: {
+                      rules: [
+                        {
+                          required: true,
+                          message: '此项为必填项',
+                        },
+                      ],
+                    },
+                  },
+                  {
+                    title: '邮箱',
+                    dataIndex: 'email',
+                    formItemProps: {
+                      rules: [
+                        {
+                          required: true,
+                          message: '此项为必填项',
+                        },
+                      ],
+                    },
+                    width: 'm',
+                  },
+                ],
+              },
+            ],
+          },
+          {
+            title: '协议',
+            dataIndex: ['project', 'license'],
+            valueType: 'select',
+            valueEnum: {
+              MIT: 'MIT',
+              BSD: 'BSD',
+              GPL: 'GPL',
+              'Apache License': 'Apache',
+            },
+            width: 'sm',
+          },
+        ]}
+      />
+      <BetaSchemaForm
+        layoutType="StepForm"
+        title="开发配置"
+        initialValues={{
+          umi: {
+            history: 'browser',
+            hash: true,
+            webpack5: false,
+            fastRefresh: false,
+            ssr: false,
+          },
+        }}
+        columns={[
+          {
+            title: '项目的路由类型',
+            dataIndex: ['umi', 'history'],
+            valueType: 'select',
+            valueEnum: {
+              browser: 'browser',
+              hash: 'hash',
+              memory: 'memory',
+            },
+            width: 'sm',
+          },
+          {
+            title: '兼容的浏览器版本',
+            dataIndex: ['umi', 'targets'],
+            valueType: 'select',
+            valueEnum: {
+              'ie 11': 'ie11',
+              现代浏览器: 'default',
+              '最新的 google': 'google',
+            },
+            width: 'sm',
+          },
+
+          {
+            title: '编译的文件增加 hash 后缀',
+            dataIndex: ['umi', 'hash'],
+            valueType: 'switch',
+          },
+          {
+            title: '是否使用webpack 5',
+            dataIndex: ['umi', 'webpack5'],
+            valueType: 'switch',
+          },
+          {
+            title: '是否使用快速刷新',
+            tooltip: '快速刷新（Fast Refresh），开发时可以保持组件状态，同时编辑提供即时反馈。',
+            dataIndex: ['umi', 'fastRefresh'],
+            valueType: 'switch',
+          },
+          {
+            title: '是否打开ssr',
+            dataIndex: ['umi', 'ssr'],
+            valueType: 'switch',
+          },
+          {
+            title: '设置浏览器的 meta 信息',
+            dataIndex: ['umi', 'metas'],
+            valueType: 'formList',
+            tooltip: `
+            示例：
+            <meta name="description" content="🍙 插件化的企业级前端应用框架。" />
+            `,
+            columns: [
+              {
+                valueType: 'group',
+                columns: [
+                  {
+                    title: '名称',
+                    dataIndex: 'name',
+                    width: 'xs',
+                    formItemProps: {
+                      rules: [
+                        {
+                          required: true,
+                          message: '此项为必填项',
+                        },
+                      ],
+                    },
+                  },
+                  {
+                    title: '内容',
+                    dataIndex: 'content',
+                    formItemProps: {
+                      rules: [
+                        {
+                          required: true,
+                          message: '此项为必填项',
+                        },
+                      ],
+                    },
+                    width: 'm',
+                  },
+                ],
+              },
+            ],
+          },
+        ]}
+      />
+    </StepsForm>
   );
 };
 
